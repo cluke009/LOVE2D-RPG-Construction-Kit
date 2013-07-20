8eebbf512d5e0a20b37983f4c3937b86b73abcda
Geraldo Ramos <gramos1984@gmail.com>
2013-07-20 15:36:31 -0400

    Revert "Override print to include tables"
    This reverts commit f521b024f28ae1043f5d665a67f15e890cbeda96.

--- globals.lua
+++ globals.lua
@@ -131,27 +131,3 @@ end
 function string.trim(s)
   return s:match'^%s*(.*%S)' or ''
 end
-
-function print(tt, indent, done)
-  done = done or {}
-  indent = indent or 0
-  if type(tt) == "table" then
-    for key, value in pairs (tt) do
-      io.write(string.rep (" ", indent)) -- indent it
-      if type (value) == "table" and not done [value] then
-        done [value] = true
-        io.write(string.format("[%s] => table\n", tostring (key)));
-        io.write(string.rep (" ", indent+4)) -- indent it
-        io.write("(\n");
-        table_print (value, indent + 7, done)
-        io.write(string.rep (" ", indent+4)) -- indent it
-        io.write(")\n");
-      else
-        io.write(string.format("[%s] => %s\n",
-            tostring (key), tostring(value)))
-      end
-    end
-  else
-    io.write(tt .. "\n")
-  end
-end
