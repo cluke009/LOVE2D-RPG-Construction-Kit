acbe08a96785188106f0b5e574a6f2b79ab762d0
Geraldo Ramos <gramos1984@gmail.com>
2013-08-16 19:18:24 -0400

    Fix menu crash when adding a party member

--- view/menu_game/menu_game_party_helper.lua
+++ view/menu_game/menu_game_party_helper.lua
@@ -6,112 +6,112 @@ local Party = Group:extend{
     end,
     onUpdate = function(self)
         if self.ready then
-        local i = 1
-        for k, hero in pairs(STATE.heroes) do
-            if hero['active'] then
-                self['image' .. i] = Tile:new{
-                    x = 0 + 340,
-                    y = 0 + 40 + (i - 1) * 100,
-                    width = hero.img.width,
-                    height = hero.img.height,
-                    image = hero.img.down.image,
-                    imageOffset = {x=0,y=196}
-                }
-                self['name' .. i] = Text:new{
-                    x = 32 + 370,
-                    y = 0 + 40 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 1, 1, 1 },
-                    font = STATE.font,
-                    text = hero.name
-                }
-                self['level' .. i] = Text:new{
-                    x = 170 + 370,
-                    y = 0 + 40 + (i - 1) * 100,
-                    width = 300,
-                    tint = { 1, 1, 1 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['hp' .. i] = Text:new{
-                    x = 32 + 370,
-                    y = 32 + 40 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 1, 1, 1 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['mp' .. i] = Text:new{
-                    x = 150 + 370,
-                    y = 32 + 40 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 1, 1, 1 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['exp' .. i] = Text:new{
-                    x = 268 + 370,
-                    y = 32 + 40 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 1, 1, 1 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['nameShadow' .. i] = Text:new{
-                    x = 32 + 372,
-                    y = 0 + 42 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 0, 0, 0 },
-                    font = STATE.font,
-                    text = hero.name
-                }
-                self['levelShadow' .. i] = Text:new{
-                    x = 170 + 372,
-                    y = 0 + 42 + (i - 1) * 100,
-                    width = 300,
-                    tint = { 0, 0, 0 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['hpShadow' .. i] = Text:new{
-                    x = 32 + 372,
-                    y = 32 + 42 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 0, 0, 0 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['mpShadow' .. i] = Text:new{
-                    x = 150 + 372,
-                    y = 32 + 42 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 0, 0, 0 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self['expShadow' .. i] = Text:new{
-                    x = 268 + 372,
-                    y = 32 + 42 + (i - 1) * 100,
-                    width = 100,
-                    tint = { 0, 0, 0 },
-                    font = STATE.font,
-                    text = ''
-                }
-                self:add(self['nameShadow' .. i])
-                self:add(self['levelShadow' .. i])
-                self:add(self['hpShadow' .. i])
-                self:add(self['mpShadow' .. i])
-                self:add(self['expShadow' .. i])
+            local i = 1
+            for k, hero in pairs(STATE.heroes) do
+                if hero['active'] then
+                    self['image' .. i] = Tile:new{
+                        x = 0 + 340,
+                        y = 0 + 40 + (i - 1) * 100,
+                        width = hero.img.width,
+                        height = hero.img.height,
+                        image = hero.img.down.image,
+                        imageOffset = {x=0,y=196}
+                    }
+                    self['name' .. i] = Text:new{
+                        x = 32 + 370,
+                        y = 0 + 40 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 1, 1, 1 },
+                        font = STATE.font,
+                        text = hero.name
+                    }
+                    self['level' .. i] = Text:new{
+                        x = 170 + 370,
+                        y = 0 + 40 + (i - 1) * 100,
+                        width = 300,
+                        tint = { 1, 1, 1 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['hp' .. i] = Text:new{
+                        x = 32 + 370,
+                        y = 32 + 40 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 1, 1, 1 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['mp' .. i] = Text:new{
+                        x = 150 + 370,
+                        y = 32 + 40 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 1, 1, 1 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['exp' .. i] = Text:new{
+                        x = 268 + 370,
+                        y = 32 + 40 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 1, 1, 1 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['nameShadow' .. i] = Text:new{
+                        x = 32 + 372,
+                        y = 0 + 42 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 0, 0, 0 },
+                        font = STATE.font,
+                        text = hero.name
+                    }
+                    self['levelShadow' .. i] = Text:new{
+                        x = 170 + 372,
+                        y = 0 + 42 + (i - 1) * 100,
+                        width = 300,
+                        tint = { 0, 0, 0 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['hpShadow' .. i] = Text:new{
+                        x = 32 + 372,
+                        y = 32 + 42 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 0, 0, 0 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['mpShadow' .. i] = Text:new{
+                        x = 150 + 372,
+                        y = 32 + 42 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 0, 0, 0 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self['expShadow' .. i] = Text:new{
+                        x = 268 + 372,
+                        y = 32 + 42 + (i - 1) * 100,
+                        width = 100,
+                        tint = { 0, 0, 0 },
+                        font = STATE.font,
+                        text = ''
+                    }
+                    self:add(self['nameShadow' .. i])
+                    self:add(self['levelShadow' .. i])
+                    self:add(self['hpShadow' .. i])
+                    self:add(self['mpShadow' .. i])
+                    self:add(self['expShadow' .. i])
 
-                self:add(self['image' .. i])
-                self:add(self['name' .. i])
-                self:add(self['level' .. i])
-                self:add(self['hp' .. i])
-                self:add(self['mp' .. i])
-                self:add(self['exp' .. i])
-                i = i + 1
+                    self:add(self['image' .. i])
+                    self:add(self['name' .. i])
+                    self:add(self['level' .. i])
+                    self:add(self['hp' .. i])
+                    self:add(self['mp' .. i])
+                    self:add(self['exp' .. i])
+                    i = i + 1
+                end
             end
-        end
             local p = 1
             for k, hero in pairs(STATE.heroes) do
                 if hero['active'] then
